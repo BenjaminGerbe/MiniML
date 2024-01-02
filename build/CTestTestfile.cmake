10781b498a8989e826ffd0b0fba489dfcb8df8f2
Benjamin <benjamin.gerbe@gmail.com>
2024-01-02 12:42:00 +0100

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/benja/Desktop/Opengl/libs/MLgine
+# Build directory: C:/Users/benja/Desktop/Opengl/libs/MLgine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
