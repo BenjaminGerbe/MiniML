9fed7116357e45f566a6f7a7ee40e512d8840779
GERBE Benjamin <bg272111@extra.cea.fr>
2024-01-22 17:11:05 +0100

    begin of lib

--- src/MiniML.hpp
+++ src/MiniML.hpp
@@ -1,10 +1,17 @@
 #pragma once
+#define MINIML_API __declspec(dllexport)
 #include "Headers/Network.hpp"
 #include <iostream>
 #include "Eigen/Dense"
 
+
 namespace MiniML{
     Network* setupXor(int i,int y,int h,int z,bool b){
+        Network* net = new Network(i,y,h,z,b);
+        return net;
+    }
+
+    extern "C" MINIML_API Network* SetUpNetwork(int i,int y,int h,int z,bool b){
         Network* net = new Network(i,y,h,z,b);
         return net;
     }
