2446af335c8ec5883cfd79d2bd4b7a8c006d6e07
LaGerbe-Bohu <benjamin.gerbe@gmail.com>
2024-01-25 11:25:41 +0100

    Optimisation of the simulation

--- src/Headers/Network.hpp
+++ src/Headers/Network.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include  <cmath>
 class Network{
+    
     std::vector<Eigen::MatrixXd> layer; 
     std::vector<Eigen::MatrixXd> delta; 
     std::vector<std::vector<Eigen::MatrixXd>> wieght; 
@@ -14,6 +15,7 @@ class Network{
     bool Regression;
     public: 
     std::vector<float> outputVector;
+    
     Network(int nbInput,int nbHidden,int heightHidden,int nboutput,bool _regression):Regression(_regression){
         std::vector<float> e;
         std::vector<float> t;
@@ -93,160 +95,20 @@ class Network{
             delta.push_back(m);
         }
     }
-    
-    Network(std::vector<float> input,  std::vector<std::vector<std::vector<float>>> w,int heidden,int heightHidden,int output,bool _regression):Network(input.size(),heidden,heightHidden,output,_regression){
-        // for (int i = 0; i < input.size(); i++)
-        // {
-        //     layer[0][i] = input[i];
-        // }
-
-        // for (int i = 0; i < w.size(); i++)
-        // {
-        //     for (int j = 0; j < w[i].size(); j++)
-        //     {
-        //         for (int k = 0; k < w[i][j].size(); k++)
-        //         {
-        //             this->wieght[i][j][k] = w[i][j][k];
-        //         }
-        //     }
-        // }
-        
-    }
 
     float GetValue(int l,int i){ return layer[l](i,0); };
-    float Network::GetWeight(int l,int i,int j){
-        int idx = l-1; // beceause the layer of the input is not added
-        if(idx < 0 || idx >= GetNetworkSize()){
-            return 0.0f;
-        }
-        return wieght[idx][i](0,j);
-    }
-
-    
-    int GetLayerSize(int l){
-        if( l < 0 || l >= layer.size()) return 0;
-        return layer[l].rows(); 
-    }
-
-    int GetLayerRealSize(int l){
-        if( l < 0 || l >= layer.size()) return 0;
-        return l==0 || l == GetNetworkSize()-2 ? layer[l].rows() -1 : layer[l].rows(); 
-    }
+    float Network::GetWeight(int l,int i,int j);
+    int GetLayerSize(int l);
+    int GetLayerRealSize(int l);
     std::vector<Eigen::MatrixXd> GetLayer(){ return this->layer;};
-
     int GetNetworkSize(){ return layer.size();}
-    void Network::SetWeight(int l,int i,int j,float v){
-        int idx = l-1; // beceause the layer of the input is not added
-        if(idx < 0){
-            throw std::invalid_argument( "the value of l can't be 0 beceause the input 0 layer is the input and input don't recieve wieght from precedent layer");
-        }
-        wieght[idx][i](0,j)= v;
-    }
-    
-    float* simulate(float* input){
+    void Network::SetWeight(int l,int i,int j,float v);
+    float* simulate(float* input);
+    float NetWorkProcess(int l,int j);
+    void backPropagation(float** input,int sizeInput,float** output,float a,int max_it);
 
-        outputVector.clear();
-        int l = GetNetworkSize();
-        for (int i = 0; i < GetLayerRealSize(0); i++)
-        {
-            layer[0](i,0) = input[i];
-        }
-
-        NetWorkProcess(l-1,0);
-        for (int i = 0; i < GetLayerSize(l-1); i++)
-        {
-            if(Regression){
-                layer[l-1](i,0) =(this->wieght[l-2][i]*layer[l-2])(0,0);
-            }
-            else{
-                layer[l-1](i,0) = sigmoid((this->wieght[l-2][i]*layer[l-2])(0,0));
-            }
-            outputVector.push_back(layer[l-1](i,0));
-        }
-        return &outputVector[0];
-    }
-
-    float sigmoid(float a){
-        return std::tanh(a);
-    }
-
-    float NetWorkProcess(int l,int j){
-        float a = 0.0;
-
-        if(l-1 > 0){
-            for (int d = 0; d < GetLayerRealSize(l-1); d++)
-            {
-                layer[l-1](d,0) = NetWorkProcess(l-1,d);
-            }
-        }
-        return sigmoid((this->wieght[l-1][j]*layer[l-1])(0,0));
-    }
-
-    void backPropagation(float** input,int sizeInput,float** output,float a,int max_it){
-    
-        float error = 0.0;
-            
-        for (int it = 0; it < max_it; it++)
-        {
-            int idx = rand() % sizeInput;
-            simulate(input[idx]);
-            float err=0.0f;
-            int w  = GetLayerRealSize(GetNetworkSize()-1);
-            for (int i = 0; i <w; i++)
-            {
-                float value = layer[GetNetworkSize()-1](i,0);
-                float b=1.0;
-                if(!Regression){
-                    b = (1-(value*value));
-                }
-                float d = b*(value - output[idx][i]);
-                this->delta[GetNetworkSize()-1](i,0) = d;
-                err+=std::abs(value - output[idx][i]);
-            }
-            error+= err/GetLayerRealSize(GetNetworkSize()-1);
-            for (int i = GetNetworkSize()-2; i >= 0; i--)
-            {
-                for (int j = 0; j < GetLayerRealSize(i); j++)
-                {
-                    float a = 0.0;
-                    for (int k = 0; k < GetLayerRealSize(i+1); k++)
-                    {
-                        a += GetWeight(i+1,k,j)*delta[i+1](k,0);
-                    }
-                    delta[i](j,0) = (1-(layer[i](j,0)*layer[i](j,0)))*a;
-                }
-                
-            }
-            
-            for (int i = 0; i < GetNetworkSize()-1; i++)
-            {
-                for (int j = 0; j < GetLayerSize(i); j++)
-                {
-                    for (int k = 0; k < GetLayerRealSize(i+1); k++)
-                    {
-                        float v = GetWeight(i+1,k,j) - a*layer[i](j,0)*delta[i+1](k,0);
-                        SetWeight(i+1,k,j,v);
-                    }
-                    
-                }
-                
-            }
-        }
-
-        Error.push_back(error/max_it);
-        float t = Iter[Iter.size()-1]+1;
-        Iter.push_back(t);
-    }
-
-    float* GetError(){
-        return &this->Error[0];
-    }
-
-    float* GetItr(){
-        return &this->Iter[0];
-    }
-
-    int GetSizeError(){
-        return this->Error.size();
-    }
+    float sigmoid(float a){ return std::tanh(a);}
+    float* GetError(){return &this->Error[0];}
+    float* GetItr(){return &this->Iter[0];}
+    int GetSizeError(){return this->Error.size();}
 };
--- src/Impl/Network.cpp
+++ src/Impl/Network.cpp
@@ -1,3 +1,119 @@
 #include "../Headers/Network.hpp"
 
+float Network::GetWeight(int l,int i,int j){
+    int idx = l-1; // beceause the layer of the input is not added
+    if(idx < 0 || idx >= GetNetworkSize()){
+        return 0.0f;
+    }
+    return wieght[idx][i](0,j);
+}
+
+int Network::GetLayerSize(int l){
+    if( l < 0 || l >= layer.size()) return 0;
+    return layer[l].rows(); 
+}
+
+int Network::GetLayerRealSize(int l){
+    if( l < 0 || l >= layer.size()) return 0;
+    return l==0 || l == GetNetworkSize()-2 ? layer[l].rows() -1 : layer[l].rows(); 
+}
+
+void Network::SetWeight(int l,int i,int j,float v){
+    int idx = l-1; // beceause the layer of the input is not added
+    if(idx < 0){
+        throw std::invalid_argument( "the value of l can't be 0 beceause the input 0 layer is the input and input don't recieve wieght from precedent layer");
+    }
+    wieght[idx][i](0,j)= v;
+}
+
+float* Network::simulate(float* input){
+
+    outputVector.clear();
+    int l = GetNetworkSize();
+    for (int i = 0; i < GetLayerRealSize(0); i++)
+    {   
+        layer[0](i,0) = input[i];
+    }
+
+    NetWorkProcess(l-1,0);
+    for (int i = 0; i < GetLayerSize(l-1); i++)
+    {
+        if(Regression){
+            layer[l-1](i,0) =(this->wieght[l-2][i]*layer[l-2])(0,0);
+        }
+        else{
+            layer[l-1](i,0) = sigmoid((this->wieght[l-2][i]*layer[l-2])(0,0));
+        }
+        outputVector.push_back(layer[l-1](i,0));
+    }
+    return &outputVector[0];
+}
+
+float Network::NetWorkProcess(int l,int j){
+    float a = 0.0;
+
+    if(l-1 > 0){
+        NetWorkProcess(l-1,0);
+        for (int d = 0; d < GetLayerRealSize(l-1); d++)
+        {
+            layer[l-1](d,0) = sigmoid((this->wieght[l-2][d]*layer[l-2])(0,0));
+        }
+    }
+    return sigmoid((this->wieght[l-1][j]*layer[l-1])(0,0));
+}
+
+void Network::backPropagation(float** input,int sizeInput,float** output,float a,int max_it){
+    
+    float error = 0.0;
+    for (int it = 0; it < max_it; it++)
+    {
+        int idx = rand() % sizeInput;
+        simulate(input[idx]);
+        float err=0.0f;
+        int w  = GetLayerRealSize(GetNetworkSize()-1);
+        for (int i = 0; i <w; i++)
+        {
+            float value = layer[GetNetworkSize()-1](i,0);
+            float b=1.0;
+            if(!Regression){
+                b = (1-(value*value));
+            }
+            float d = b*(value - output[idx][i]);
+            this->delta[GetNetworkSize()-1](i,0) = d;
+            err+=std::abs(value - output[idx][i]);
+        }
+        error+= err/GetLayerRealSize(GetNetworkSize()-1);
+        for (int i = GetNetworkSize()-2; i >= 0; i--)
+        {
+            for (int j = 0; j < GetLayerRealSize(i); j++)
+            {
+                float a = 0.0;
+                for (int k = 0; k < GetLayerRealSize(i+1); k++)
+                {
+                    a += GetWeight(i+1,k,j)*delta[i+1](k,0);
+                }
+                delta[i](j,0) = (1-(layer[i](j,0)*layer[i](j,0)))*a;
+            }
+            
+        }
+        
+        for (int i = 0; i < GetNetworkSize()-1; i++)
+        {
+            for (int j = 0; j < GetLayerSize(i); j++)
+            {
+                for (int k = 0; k < GetLayerRealSize(i+1); k++)
+                {
+                    float v = GetWeight(i+1,k,j) - a*layer[i](j,0)*delta[i+1](k,0);
+                    SetWeight(i+1,k,j,v);
+                }
+                
+            }
+            
+        }
+    }
+
+    Error.push_back(error/max_it);
+    float t = Iter[Iter.size()-1]+1;
+    Iter.push_back(t);
+}
 
